45201b8b0ce40296b0a3aad20476f1114a1a2c8b
r2mzes <r2mzes@gmail.com>
2014-05-04 21:44:29 +0200

    database model

---
+++ model/poll.js
@@ -0,0 +1,37 @@
+// The Poll model
+
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+    , ObjectId = Schema.ObjectId;
+
+var pollSchema = new Schema({
+    poll_id: ObjectId,
+    name: String,
+    description: {type: String, "default": ""},
+    start_time: Date,
+    end_time: Date,
+    declaration_end_time: Date,
+    is_declaration_closed: {type: Boolean, "default":false},
+    creator_id: {type:String, "default": null},
+    creator_name: {type:String, "default":"guest"},
+    creator_mail: String,
+    creation_token: String,
+    required_groups: {type: Array, "default": []}
+});
+
+
+pollSchema.pre('save', function(next){
+    this.creation_token = pollSchema.generateCreationToken();
+    next();
+})
+
+pollSchema.virtual('id')
+    .get(function () {
+        return this._id.toHexString();
+    });
+
+pollSchema.generateCreationToken = function(){
+    return "WE1243HSKD";
+}
+
+module.exports = mongoose.model('Poll', pollSchema);
