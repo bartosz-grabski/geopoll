f1f4ca4a2f71788bae1cd894bb317ca33d2deee6
Bartosz Grabski <b.grabski92@gmail.com>
2014-05-04 18:57:51 +0200

    further ui changes, added confirmation modal, added datepicker (to be modified later)

--- static/js/controllers/PollController.js
+++ static/js/controllers/PollController.js
@@ -1,6 +1,77 @@
-controllers.controller('PollController', function ($scope, $rootScope, $location) {
+controllers.controller('PollController', function ($scope, $rootScope, $location, $modal,$log) {
 
-    $scope.submit = function () {
-    	//TODO
+	$scope.submit = function () {
+		confirmModalCreate($modal,$scope,$log);
+	};
+
+
+    $scope.today = function() {
+    	$scope.dt = new Date();
     };
+    $scope.today();
+
+    $scope.clear = function () {
+    	$scope.dt = null;
+    };
+
+  	// Disable weekend selection
+  	$scope.disabled = function(date, mode) {
+  		return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  	};
+
+  	$scope.toggleMin = function() {
+  		$scope.minDate = $scope.minDate ? null : new Date();
+  	};
+  	$scope.toggleMin();
+
+  	$scope.open = function($event) {
+  	$event.preventDefault();
+  	$event.stopPropagation();
+
+  	$scope.opened = true;
+  	};
+
+  	$scope.dateOptions = {
+  		formatYear: 'yy',
+  		startingDay: 1
+  	};
+
+  	$scope.initDate = new Date('2016-15-20');
+  	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  	$scope.format = $scope.formats[0];
+
 });
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, poll) {
+
+	$scope.poll = poll;
+    $scope.ok = function () {
+    	$modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+    	$modalInstance.dismiss('cancel');
+    };
+};
+
+var confirmModalCreate = function($modal,$scope,$log) {
+		
+		var modalInstance = $modal.open({
+			templateUrl: 'confirm.html',
+			controller: ModalInstanceCtrl,
+			resolve: {
+				poll : function () {
+					return { title: $scope.pollName, description: $scope.pollDesc };
+				}
+			}
+		});
+
+		modalInstance.result.then(function (selectedItem) {
+			$scope.selected = selectedItem;
+		}, function () {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
+
+
+
+}
