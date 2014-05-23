b2dbf8cf37e84d134ba64e2ad51830695bc8db10
Bartosz Grabski <b.grabski92@gmail.com>
2014-05-23 17:18:50 +0200

    Added timeline, work in progress

--- static/js/controllers/PollController.js
+++ static/js/controllers/PollController.js
@@ -1,4 +1,4 @@
-controllers.controller('PollController', function ($scope, $rootScope, $location, pollService, modalService,$routeParams) {
+controllers.controller('PollController', function ($scope, $rootScope, $location, $window, pollService, modalService,$routeParams) {
 
 	$scope.messages = {
 		"pollUpdateError" : "There were problems updating your poll!",
@@ -51,6 +51,36 @@ controllers.controller('PollController', function ($scope, $rootScope, $location
 	}
 
 	$scope.getPollInfo();
+
+
+	var tl;
+	function onLoad() {
+		var bandInfos = [
+		$window.Timeline.createBandInfo({
+			width:          "70%", 
+			intervalUnit:   Timeline.DateTime.MONTH, 
+			intervalPixels: 100
+		}),
+		$window.Timeline.createBandInfo({
+			width:          "30%", 
+			intervalUnit:   Timeline.DateTime.YEAR, 
+			intervalPixels: 200
+		})
+		];
+		tl = $window.Timeline.create(document.getElementById("my-timeline"), bandInfos);
+	}
+
+	var resizeTimerID = null;
+	function onResize() {
+		if (resizeTimerID == null) {
+			resizeTimerID = $window.setTimeout(function() {
+				resizeTimerID = null;
+				tl.layout();
+			}, 500);
+		}
+	}
+
+	onLoad();
 
 
 });
