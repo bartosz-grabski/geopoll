82d1d04358aea124123b78934e0c17b32f470852
Bartosz Grabski <b.grabski92@gmail.com>
2014-08-21 03:22:52 +0200

    Node debugger added. Please use Chrome browser to navigate to debugger page. Also please do npm install before using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,27 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-node-inspector');
+    grunt.loadNpmTasks('grunt-concurrent');
 
     grunt.initConfig({
+        concurrent: {
+            dev: {
+                tasks: ['nodemon:debug','node-inspector'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
         nodemon: {
             dev: {
                 script: 'app.js'
+            },
+            debug: {
+                script: 'app.js',
+                options: {
+                    nodeArgs: ['--debug']
+                }
             }
         },
         jshint: {
@@ -14,9 +30,13 @@ module.exports = function(grunt) {
         		force : true
         	},
         	myFiles : ['model/*.js','email/*.js','db/*.js','routes/*.js','static/js/**/*.js']
+        },
+        'node-inspector': {
+            dev: {}
         }
     });
 
-    grunt.registerTask('default', ['jshint','nodemon']);
-
+    grunt.registerTask('jshint', ['jshint']);
+    grunt.registerTask('debug', ['concurrent']);
+    grunt.registerTask('default',['nodemon:dev']);
 }
