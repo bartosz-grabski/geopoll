8841e83b6178885fd58711dfbff8e5154eb37fdf
r2mzes <r2mzes@gmail.com>
2014-08-26 01:07:57 +0200

    sending registration form data into controller + lame validation added

--- static/js/controllers/RegisterController.js
+++ static/js/controllers/RegisterController.js
@@ -1,3 +1,17 @@
-controllers.controller('RegisterController', function($scope, $rootScope){
+controllers.controller('RegisterController', function ($scope, registerService) {
 
+    messages = {
+        "userRegistrationMessageSuccess": "Successfully registered new user!",
+        "userRegistrationMessageError": "Error registering new user!"
+    };
+
+    $scope.isInvalida=false;
+    $scope.usernameChange = function () {
+        $scope.isInvalid = false;
+    }
+
+    $scope.registration = {};
+    $scope.saveData = function () {
+        console.log($scope.registration);
+    }
 });
--- static/js/services/RegisterService.js
+++ static/js/services/RegisterService.js
@@ -1,3 +1,19 @@
 services.factory('registerService', function(){
 
-})
+    var service = {};
+
+    /**
+     *	Sends post request to create a new poll, handles response
+     *
+     */
+    service.registerNewUser = function(data,onSuccess, onFailure) {
+
+        $http.post('/register',data)
+            .success(onSuccess)
+            .error(onFailure);
+    };
+
+
+    return service;
+
+});
