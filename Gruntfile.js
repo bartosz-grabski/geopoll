31133bfd6beb1442b4800f5691c809d1c83aabb4
Bartosz Grabski <b.grabski92@gmail.com>
2014-09-12 13:09:39 +0200

    added grunt minifying capabilites

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-node-inspector');
     grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-usemin");
+    grunt.loadNpmTasks("grunt-rev");
+
 
     grunt.initConfig({
         concurrent: {
@@ -33,10 +41,54 @@ module.exports = function(grunt) {
         },
         'node-inspector': {
             dev: {}
+        },
+        clean: {
+            main: ["build", '.tmp'],
+            build: ["build/static/js","build/static/css","build/static/lib",
+                '.tmp'],
+            dist: ["dist"]
+        },
+        copy: {
+            main: {
+                expand: true,
+                src: [ "**","!node_modules/"],
+                dest: "build/"
+            },
+            dist: {
+                expand: true,
+                cwd: "dist",
+                src: ["**"],
+                dest: "build/static"
+            }
+        },
+        rev: {
+            files: {
+                src: ["build/**/**/*.{js.css}"]
+            }
+        },
+
+        useminPrepare: {
+            html: "build/views/index.html",
+            options: {
+                root: "static"
+            }
+        },
+        usemin: {
+            html: ["build/views/index.html"]
+        },
+        uglify: {
+            options: {
+                report: "min",
+                mangle: false
+            }
         }
+
     });
 
     grunt.registerTask('jshint', ['jshint']);
     grunt.registerTask('debug', ['concurrent']);
     grunt.registerTask('default',['nodemon:dev']);
+    grunt.registerTask("minify", [
+        "copy:main", "useminPrepare", "concat", "uglify", "cssmin", "rev", "usemin", "clean:build", "copy:dist", "clean:dist"
+    ]);
 }
