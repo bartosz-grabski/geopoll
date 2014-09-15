5e19780cf1fc9366c1845f40e3bf53caec6db2f9
r2mzes <r2mzes@gmail.com>
2014-09-15 21:30:23 +0200

    not showing poll table if there is no poll to list

--- static/js/controllers/UserPollsController.js
+++ static/js/controllers/UserPollsController.js
@@ -1,10 +1,14 @@
 controllers.controller('UserPollsController', function ($scope, userPollsService) {
 
     $scope.polls;
+    $scope.thereIsAtLeastOneUserPoll = false;
 
     userPollsService.getUserPolls(function (res) {
         console.log(res);
         $scope.polls = res;
+        if (res.length > 0) {
+            $scope.thereIsAtLeastOneUserPoll = true;
+        }
     }, function () {
         console.log("Cannot load user polls");
     });
